dd09ae755d5e4126a87c39ee155d8e3ba47cfdc4
Your Name <sairamkallakuri9@gmail.com>
2021-03-11 19:02:54 +0530

    HIS-134 fix provided

--- src/main/java/com/sai/Application.java
+++ src/main/java/com/sai/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		//HIS FOR -134
 		run.close();
 		
 		
